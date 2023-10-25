46c0f43cad95f5b6b6686ea87d5787e0bd6beea6
PhuongLoc <103478293+PhuongLoc@users.noreply.github.com>
2023-10-25 13:40:25 +0700

    commit module 3

--- wordpress/wp-content/themes/twentytwentyone/header.php
+++ wordpress/wp-content/themes/twentytwentyone/header.php
@@ -17,6 +17,10 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<link rel="stylesheet" href=".\wp-content\style.css">
 	<?php wp_head(); ?>
 </head>
 
