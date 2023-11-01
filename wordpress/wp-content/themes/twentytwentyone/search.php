fdb231d6d83d21eddff21222568ce6e39757f2a5
thanhluan2706 <21211tt3881@mail.tdc.edu.vn>
2023-11-01 13:32:19 +0700

    update source v2

---
+++ wordpress/wp-content/themes/twentytwentyone/search.php
@@ -0,0 +1,111 @@
+<?php
+
+/**
+ * The template for displaying search results pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+get_header();
+?>
+<div class="row" style="display: flex;">
+	<div class="col-md-3 col-bg">
+		<p class="title-tt">Trang Mới Nhất</p>
+		<div class="border-tt">
+			<br>
+		</div>
+		<div class='page-class'>
+			<?php
+			// Lấy danh sách các bài viết
+			$args = array(
+				'post_type' => 'Page %', // Loại bài viết bạn muốn lấy
+				'posts_per_page' => -1, // -1 để lấy tất cả các bài viết
+			);
+			$query = new WP_Query($args);
+			// Kiểm tra xem có bài viết nào không
+			if ($query->have_posts()) {
+				// Bắt đầu vòng lặp
+				while ($query->have_posts()) {
+					$query->the_post();
+					// Lấy tiêu đề của bài viết
+					$title = sprintf('<h4 class="title-class">%s</h4>', get_the_title());
+					// Lấy nội dung của bài viết
+					$content = sprintf('<div class="content-class">%s</div>', get_post_field('post_content'));
+					// Hiển thị tiêu đề và nội dung
+					echo $title;
+					echo '<hr width="45px;" 
+					size="10" style ="
+					margin: 0;"> ';
+					echo $content;
+				}
+				// Đặt lại trạng thái của vòng lặp
+				wp_reset_postdata();
+			} else {
+				// Không có bài viết nào được tìm thấy
+				echo 'Không có bài viết nào.';
+			}
+			?>
+		</div>
+	</div>
+	<div class="col-md-9">
+		<?php
+		if (have_posts()) {
+		?>
+			<header class="page-header alignwide">
+				<h1 class="page-title result-tt">
+					<?php
+					printf(
+						/* translators: %s: Search term. */
+						esc_html__('Results for "%s"', 'twentytwentyone'),
+						'<span class="page-description search-term">' . esc_html(get_search_query()) . '</span>'
+					);
+					?>
+				</h1>
+			</header><!-- .page-header -->
+
+			<div class="search-result-count default-max-width col-md-9 search-css">
+				<?php
+				printf(
+					esc_html(
+						/* translators: %d: The number of search results. */
+						_n(
+							'We found %d result for your search.',
+							'We found %d results for your search.',
+							(int) $wp_query->found_posts,
+							'twentytwentyone'
+						)
+					),
+					(int) $wp_query->found_posts
+				);
+				?>
+			</div><!-- .search-result-count -->
+			<?php
+			// Start the Loop.
+			while (have_posts()) {
+				the_post();
+
+				/*
+         * Include the Post-Format-specific template for the content.
+         * If you want to override this in a child theme, then include a file
+         * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+         */
+				get_template_part('template-parts/content/content-excerpt', get_post_format());
+			} // End the loop.
+
+			// Previous/next page navigation.
+			twenty_twenty_one_the_posts_navigation();
+
+			// If no content, include the "No posts found" template. 
+			?>
+	</div>
+</div>
+<?php
+		} else {
+			get_template_part('template-parts/content/content-none');
+		}
+
+		get_footer();
---
+++ wordpress/wp-content/themes/twentytwentyone/template-parts/content/content-excerpt.php
@@ -0,0 +1,482 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+</script>
+
+
+<!--[if lt IE 9]>
+        <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,700,800|Open+Sans+Condensed:300,700|Prata&subset=vietnamese' rel='stylesheet' type='text/css'>
+
+<link rel="stylesheet" href="http://fit.tdc.edu.vn/addons/default/themes/bootstrapThree/css/bootstrap.min.css" />
+<link rel="stylesheet" href="http://fit.tdc.edu.vn/addons/default/themes/bootstrapThree/css/font-awesome.min.css" />
+<!-- <link rel="stylesheet" href="http://fit.tdc.edu.vn/addons/default/themes/bootstrapThree/css/style.css" /> -->
+<link rel="stylesheet" href="http://fit.tdc.edu.vn/addons/default/themes/bootstrapThree/css/responsive-slider.css" />
+<script src="http://fit.tdc.edu.vn/addons/default/themes/bootstrapThree/js/jquery.min.js"></script>
+<script src="http://fit.tdc.edu.vn/addons/default/themes/bootstrapThree/js/bootstrap.min.js"></script>
+<script src="http://fit.tdc.edu.vn/addons/default/themes/bootstrapThree/js/jquery.event.move.js"></script>
+<script src="http://fit.tdc.edu.vn/addons/default/themes/bootstrapThree/js/responsive-slider.min.js"></script>
+<script src="http://fit.tdc.edu.vn/addons/default/themes/bootstrapThree/js/sitescript.js"></script>
+
+<link rel="canonical" href="http://fit.tdc.edu.vn/tin-tuc" />
+<link rel="alternate" type="application/rss+xml" title="Khoa Công nghệ thông tin | Cao đẳng Công nghệ Thủ Đức | FIT - TDC" href="http://fit.tdc.edu.vn/blog/rss/all.rss" />
+<link rel="stylesheet" href="http://fit.tdc.edu.vn/addons/shared_addons/modules/contents/css/sample.css" />
+<script src="http://fit.tdc.edu.vn/addons/shared_addons/modules/contents/js/sample.js"></script>
+
+
+
+
+
+<!-- <style>
+
+	/* Module 2 */
+	.list_news {
+		margin-top: 45px;
+		margin: 45px 0;
+	}
+
+	.list_new_view {
+		margin-bottom: 15px;
+	}
+
+	.top_news_block_desc {
+		background: #fff;
+		-webkit-box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.2);
+		-moz-box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.2);
+		box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.2);
+		font-size: 0.9em;
+		padding: 15px;
+	}
+
+	.top_news_block_desc h5 {
+		text-transform: uppercase;
+		font-family: 'Open Sans Condensed', sans-serif;
+		font-weight: bold;
+	}
+
+	.topnewstime {
+		text-align: center;
+		margin-top: 12px;
+		text-align: center;
+		padding-left: 0;
+		padding-left: 15px;
+		padding-left: 0;
+	}
+
+	.topnewsdate {
+		font-family: 'Prata', serif;
+		font-size: 3.1em;
+		line-height: 1em;
+		margin-left: 15px;
+	}
+
+	.topnewsmonth {
+		text-transform: uppercase;
+		font-size: 0.9em;
+		margin-left: 15px;
+	}
+
+	.shortdesc {
+		border-left: 1px solid #666;
+		padding-right: 30px;
+	}
+
+	.hcontent {
+		margin-top: 15px;
+	}
+
+	a {
+		text-decoration: none;
+		color: #428bca;
+	}
+
+
+	/* -------------------------------------------- */
+	/*	12. Comments			
+/* -------------------------------------------------------------------------- */
+
+
+	/* Comment Headers ----------------------------- */
+
+	.comments-wrapper {
+		margin-top: 5rem;
+	}
+
+	.comment-reply-title {
+		margin: 0 0 4rem 0;
+		text-align: center;
+	}
+
+
+	/* Comment Item ----------------------------- */
+
+	/* COMMENT HEADER */
+
+	.comments .comment,
+	.comments .pingback,
+	.comments .trackback,
+	.comments .review {
+		padding-top: 3.5rem;
+	}
+
+	div.comment:first-of-type {
+		margin-top: 3.5rem;
+		padding-top: 0;
+	}
+
+	.comments .comments-header+div {
+		margin-top: 0;
+		padding-top: 0;
+	}
+
+	.comment-body {
+		position: relative;
+	}
+
+	.comment .comment {
+		padding-left: 5%;
+	}
+
+	.comment-meta {
+		line-height: 1.1;
+		margin-bottom: 1.5rem;
+		min-height: 5rem;
+		padding-left: 5rem;
+		position: relative;
+	}
+
+	.hide-avatars .comment-meta {
+		min-height: 0;
+		padding-left: 0;
+	}
+
+	.comment-meta a {
+		color: inherit;
+	}
+
+	.comment-author {
+		font-size: 1.8rem;
+		font-weight: 700;
+		letter-spacing: -0.027777778em;
+	}
+
+	.comment-author a {
+		text-decoration: underline;
+	}
+
+	.comment-author a:hover,
+	.comment-author a:focus {
+		text-decoration: none;
+	}
+
+	.comment-meta .avatar {
+		height: 4rem;
+		position: absolute;
+		left: 0;
+		top: 0;
+		width: 4rem;
+	}
+
+	.comment-author .url {
+		text-decoration: underline;
+	}
+
+	.comment-metadata {
+		color: #6d6d6d;
+		font-size: 1.4rem;
+		font-weight: 500;
+		margin-top: 0.6rem;
+	}
+
+	.comment-metadata a {
+		text-decoration: none;
+	}
+
+	.comment-metadata a:focus,
+	.comment-metadata a:hover {
+		text-decoration: underline;
+	}
+
+
+	/* COMMENT CONTENT */
+
+	.comment-content.entry-content>*:not(.alignwide):not(.alignfull):not(.alignleft):not(.alignright):not(.is-style-wide) {
+		width: 100%;
+	}
+
+	.comment-content.entry-content .alignleft {
+
+		/*rtl:ignore*/
+		margin-left: 0;
+	}
+
+	.comment-content.entry-content .alignright {
+
+		/*rtl:ignore*/
+		margin-right: 0;
+	}
+
+	/* COMMENT FOOTER */
+
+	.comment-footer-meta {
+		align-items: center;
+		display: flex;
+		flex-wrap: wrap;
+		font-size: 1.2rem;
+		font-weight: 600;
+		justify-content: flex-start;
+		letter-spacing: 0.030833333em;
+		line-height: 1;
+		margin: 1.5rem 0 -1rem -1.5rem;
+		text-transform: uppercase;
+	}
+
+	.comment-footer-meta>* {
+		margin: 0 0 1rem 1.5rem;
+	}
+
+	.comment-reply-link {
+		background-color: #cd2653;
+		color: #fff;
+		display: block;
+		padding: 0.7rem;
+	}
+
+	.bypostauthor .comment-footer-meta .by-post-author {
+		display: block;
+	}
+
+	.comment-footer-meta a {
+		text-decoration: none;
+	}
+
+	.comment-footer-meta a:focus,
+	.comment-footer-meta a:hover {
+		text-decoration: underline;
+	}
+
+
+	/* Pingbacks & Trackbacks ------------------------- */
+
+	.pingback .comment-meta,
+	.trackback .comment-meta {
+		padding-left: 0;
+	}
+
+
+	/* Comments Pagination ---------------------------- */
+
+	.comments-pagination {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-between;
+		margin-top: 6rem;
+		text-align: center;
+	}
+
+	.comments-pagination.only-next {
+		justify-content: flex-end;
+	}
+
+	.comments-pagination .page-numbers {
+		display: none;
+		text-decoration: none;
+	}
+
+	.comments-pagination .page-numbers:focus,
+	.comments-pagination .page-numbers:hover {
+		text-decoration: underline;
+	}
+
+	.comments-pagination .prev,
+	.comments-pagination .next {
+		display: block;
+	}
+
+	.comments-pagination .prev {
+		left: 0;
+	}
+
+	.comments-pagination .next {
+		right: 0;
+		text-align: right;
+	}
+
+
+	/* Comment Respond ---------------------------- */
+
+	.comment-respond::after {
+		clear: both;
+		content: "";
+		display: block;
+	}
+
+	.comment-respond .comment-notes,
+	.comment-respond .logged-in-as {
+		color: #6d6d6d;
+		font-size: 1.6rem;
+		line-height: 1.4;
+		margin: -3rem 0 4rem 0;
+		text-align: center;
+	}
+
+	.comment-respond .comment-notes a,
+	.comment-respond .logged-in-as a {
+		color: inherit;
+		text-decoration: none;
+	}
+
+	.comment-respond .comment-notes a:focus,
+	.comment-respond .comment-notes a:hover,
+	.comment-respond .logged-in-as a:focus,
+	.comment-respond .logged-in-as a:hover {
+		text-decoration: underline;
+	}
+
+	.comment-respond p {
+		line-height: 1.1;
+		margin-bottom: 2rem;
+		margin-left: auto;
+		margin-right: auto;
+	}
+
+	.comment-respond p:not(.comment-notes) {
+		max-width: 58rem;
+	}
+
+	.comment-form-cookies-consent {
+		align-items: baseline;
+		display: flex;
+	}
+
+	.comment-respond>p:last-of-type {
+		margin-bottom: 0;
+	}
+
+	.comment-respond label {
+		display: block;
+	}
+
+	.comment-respond input[type="checkbox"]+label {
+		font-size: 1.5rem;
+		line-height: 1.25;
+	}
+
+	.comment-respond input[type="text"],
+	.comment-respond input[type="email"],
+	.comment-respond textarea {
+		margin-bottom: 0;
+	}
+
+	.comment-respond textarea {
+		height: 15rem;
+	}
+
+	.comment-respond #submit {
+		display: block;
+	}
+
+	.comment-respond .comments-closed {
+		text-align: center;
+	}
+
+
+	/* Reply Respond ---------------------------- */
+
+	.comments .comment-respond {
+		padding: 3rem 0 0;
+	}
+
+	.comments .comment-respond .comment-reply-title,
+	.comments .comment-respond .comment-notes,
+	.comments .comment-respond .logged-in-as {
+		text-align: left;
+	}
+
+	.comment-reply-title small {
+		display: block;
+		font-size: 1.6rem;
+		font-weight: 600;
+		letter-spacing: -0.0277em;
+		margin: 0.5rem 0 0 0;
+		white-space: nowrap;
+	}
+
+	.comment-reply-title small a {
+		text-decoration: none;
+	}
+
+	.comment-reply-title small a:focus,
+	.comment-reply-title small a:hover {
+		text-decoration: underline;
+	}
+</style> -->
+
+<style>
+	.line{
+		width: 50px;
+	}
+</style>
+<?php
+/**
+ * Template part for displaying post archives and search results
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<div class="list_news">
+		<div class="list_new_view">
+			<div class="row">
+				<div class="col-md-3">
+				</div>
+				<div class="col-md-6 top_news_block_desc">
+					<div class="row">
+						<div class="col-md-3 col-xs-3 topnewstime">
+							<span class="topnewsdate"><?php echo get_the_time('d') ?></span><br>
+							<span class="topnewsmonth">Tháng <?php echo get_the_time('m') ?></span><br>
+						</div>
+						<div class="col-md-9 col-xs-9 shortdesc">
+							<h5>
+								<a href=""><?php get_template_part('template-parts/header/excerpt-header', get_post_format()); ?></a>
+							</h5>
+							<p><?php get_template_part('template-parts/excerpt/excerpt', get_post_format()); ?>
+						</div>
+					</div>
+				</div>
+				<!-- <div class="col-md-3">
+				</div> -->
+				<div class="col-md-3 col-sm-6 ">
+					<div class="vc_column-inner ">
+						<div class="wpb_wrapper">
+							<div class="wpb_text_column wpb_content_element ">
+								<div class="wpb_wrapper">
+									<h4>Comment</h4>
+									<hr class="line">
+									<ul class="list_1">
+										<a href="http://fit.tdc.edu.vn/tuyen-sinh/nganh-tin-hoc-ung-dung">Bài viết hay quá</a>
+										<br>
+										<a href="http://fit.tdc.edu.vn/tuyen-sinh/nganh-truyen-thong-va-mang-may-tinh-tc">Cảm ơn tác giả</a>
+										<br>
+										<a href="http://fit.tdc.edu.vn/tuyen-sinh/nganh-truyen-thong-da-phuong-tien">Bài viết thật hữu </a>
+									</ul>
+
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</article><!-- #post-${ID} -->
+
+
